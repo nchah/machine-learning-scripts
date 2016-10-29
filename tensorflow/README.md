23d1168723c5cbcc2ea248a3f27254b25b5f4d50
Niel C <nielchah@gmail.com>
2016-10-29 00:28:55 -0400

    Added README, expert script

---
+++ tensorflow/README.md
@@ -0,0 +1,63 @@
+# nchah/machine-learning-scripts/tensorflow
+
+## tensorflow-mnist
+
+Running the tutorial scripts on the command line.
+
+```
+$ source activate tensorflow
+
+# The sample code as is
+(tensorflow) $ python mnist_softmax.py
+Extracting /tmp/tensorflow/mnist/input_data/train-images-idx3-ubyte.gz
+Extracting /tmp/tensorflow/mnist/input_data/train-labels-idx1-ubyte.gz
+Extracting /tmp/tensorflow/mnist/input_data/t10k-images-idx3-ubyte.gz
+Extracting /tmp/tensorflow/mnist/input_data/t10k-labels-idx1-ubyte.gz
+0.918
+
+# The same code but written while going through the guide
+(tensorflow) $ python mnist_softmax_beginner.py
+Extracting MNIST_data/train-images-idx3-ubyte.gz
+Extracting MNIST_data/train-labels-idx1-ubyte.gz
+Extracting MNIST_data/t10k-images-idx3-ubyte.gz
+Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
+MNIST data loaded.
+Starting to build the TensorFlow computation graph...
+Starting training stage...
+Training the model 1000 times...
+Evaluating the model...
+Accuracy: 0.9154
+
+# The Expert tutorial where a multilayer convolutional network is added
+(tensorflow) $ python mnist_softmax_expert.py
+Extracting MNIST_data/train-images-idx3-ubyte.gz
+Extracting MNIST_data/train-labels-idx1-ubyte.gz
+Extracting MNIST_data/t10k-images-idx3-ubyte.gz
+Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
+MNIST data loaded.
+Starting to build the TensorFlow computation graph...
+Starting training stage...
+Training the model 1000 times...
+Evaluating the model...
+Accuracy: 0.9154
+Implementing Multilayer Convolutional Network to improve accuracy.
+Training the model...
+step 0, training accuracy 0.14
+step 100, training accuracy 0.82
+step 200, training accuracy 0.9
+step 300, training accuracy 0.92
+step 400, training accuracy 0.98
+step 500, training accuracy 0.94
+step 600, training accuracy 0.92
+step 700, training accuracy 0.96
+step 800, training accuracy 0.94
+step 900, training accuracy 0.96
+step 1000, training accuracy 0.94
+step 1100, training accuracy 0.94
+step 1200, training accuracy 1
+step 1300, training accuracy 0.98
+...
+
+```
+
+
--- tensorflow/tensorflow-mnist/mnist_softmax_expert.py
+++ tensorflow/tensorflow-mnist/mnist_softmax_expert.py
@@ -67,3 +67,95 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Resulting accuracy of the model
 print("Accuracy: " + str(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})))
+
+
+print("Implementing Multilayer Convolutional Network to improve accuracy.")
+
+# Implementing a Multilayer Convolutional Network
+
+# Weight Initialization
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+# Convolution and Pooling
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# 1st Convolutional layer
+
+# This layer will compute 32 features for each 5x5 patch
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+# reshape x to a 4d tensor. 2nd, 3rd dimensions = width and height, 4th = number of color channels
+x_image = tf.reshape(x, [-1,28,28,1])
+
+# convoluate x_image with the Weight tensor, add bias, apply ReLU function, and max pool
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# 2nd Convolutional layer
+
+# This layer has 64 features for each 5x5 patch
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# Densely connected layer
+
+# Image has been reduced to 7x7 so now add 1024 neuron layer to process full image
+# reshape tensor from the pooling layer into a batch of vectors, multiply by weight matrix, add bias, and apply ReLU.
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# Dropout
+
+# Apply dropout before the readout layer to reduce overfitting
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# Readout Layer
+
+# The final layer for the softmax regression
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+
+print("Training the model...")
+
+# This step does 20,000 training iterations and may take up to 30 mins depending on local setup
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.initialize_all_variables())
+for i in range(20000):
+    batch = mnist.train.next_batch(50)
+    if i%100 == 0:
+        train_accuracy = accuracy.eval(session=sess, feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+        print("step %d, training accuracy %g"%(i, train_accuracy))
+    train_step.run(session=sess, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
