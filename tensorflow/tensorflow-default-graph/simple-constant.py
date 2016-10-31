7b5f7df3c3cff35ad7f5137e863efdb044d5286c
Niel C <nielchah@gmail.com>
2016-10-31 18:51:08 -0400

    Updated dir structure

---
+++ tensorflow/tensorflow-default-graph/simple-constant.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+
+# 1. TensorFlow Graph - first creating the computation graph
+
+# Starting off with the default graph
+graph = tf.get_default_graph()
+
+# Currently there are no operations specified in the graph
+graph.get_operations()
+
+# This sets a Constant of 1.0 as a node/operation in the graph
+input_value = tf.constant(1.0)
+
+operations = graph.get_operations()
+
+# Showing the protocol buffer for that first node
+operations[0].node_def
+
+
+# 2. Sessions - computing in Sessions
+
+# Start the sesison and print out the input_value Constant
+sess = tf.Session()
+sess.run(input_value)
+
